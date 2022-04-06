7ff1e242e9b9ac376c4c0e7e72fd1e442385f49d
Martin Hazelton <36397695+MartinLHazelton@users.noreply.github.com>
2022-04-06 13:01:28 +1200

    Add files via upload

--- R/plotnet.r
+++ R/plotnet.r
@@ -14,6 +14,7 @@
 #' @param LINK.LABEL Label links? Defaults to TRUE.
 #' @param RightAbove Further control for link label positions. Defaults to FALSE, which means that labels appear to left looking in direction of arrows. If TRUE, labels appear to the left/above links.
 #' @param link.lty Line type for links: single value, or vector (lty per link). Defaults to 1.
+#' @param link.col Line type for links: single value, or vector (lty per link). Defaults to 1 (black).
 #' @param node.lwd Line width for nodes: single value, or vector (lty per nodes). Defaults to 2. 
 #' @return Produces a plot of the network
 #' @keywords network plotting
@@ -24,13 +25,17 @@
 #' link.node.matrix <- rbind(c(1,2),c(1,4),c(2,5),c(3,2),c(3,6),c(4,5),c(6,5))
 #' plotnet(nodes.x,nodes.y,link.node.matrix,ARROW.LENGTH=0.3,link.sep=0,radius=0.7,RightAbove=T)
 
-plotnet <- function(nodes.x,nodes.y,link.node.matrix,radius=1/2,link.sep=1/12,link.label.offset=0.4,LABEL.CEX=1,ARROW.LENGTH=0.4,ARR.TYPE="triangle",node.label=TRUE,LINK.LABEL=TRUE,RightAbove=F,link.lty=1,node.lwd=2){
+plotnet <- function(nodes.x,nodes.y,link.node.matrix,radius=1/2,link.sep=1/12,link.label.offset=0.4,LABEL.CEX=1,ARROW.LENGTH=0.4,ARR.TYPE="triangle",node.label=TRUE,LINK.LABEL=TRUE,RightAbove=F,link.lty=1,link.col=1,node.lwd=2){
   require(shape)
   n.nodes <- length(nodes.x)
   n.links <- nrow(link.node.matrix)
   if (length(link.sep) < n.links) link.sep <- rep(link.sep,length=n.links)
+  if (length(link.lty) < n.links) link.lty <- rep(link.lty,length=n.links)
+  if (length(link.col) < n.links) link.col <- rep(link.col,length=n.links)
   plot(0,0,xlim=c(min(nodes.x)-radius,max(nodes.x)+radius),ylim=c(min(nodes.y)-radius,max(nodes.y)+radius),type="n",axes=F,xlab="",ylab="")
-  symbols(nodes.x,nodes.y,circles=rep(radius,n.nodes),add=T,inches=FALSE,lwd=node.lwd) 
+  for (i in 1:n.nodes){
+	symbols(nodes.x[i],nodes.y[i],circles=radius,add=T,inches=FALSE,lwd=node.lwd[i]) 
+  }
   if (node.label) text(nodes.x,nodes.y,as.character(1:n.nodes),cex=LABEL.CEX,font=2)
   for (i in 1:n.links){
     from.node <- link.node.matrix[i,1]
@@ -50,7 +55,7 @@ plotnet <- function(nodes.x,nodes.y,link.node.matrix,radius=1/2,link.sep=1/12,li
     x1 <- from.x + radius*1.1*cos(theta1)
     x2 <- to.x - radius*1.1*cos(theta2)
     
-    Arrows(x1,y1,x2,y2,lwd=1,arr.adj=1,arr.length=ARROW.LENGTH,arr.type=ARR.TYPE,lty=link.lty)
+    Arrows(x1,y1,x2,y2,lwd=1,arr.adj=1,arr.length=ARROW.LENGTH,arr.type=ARR.TYPE,lty=link.lty[i],col=link.col[i])
     
     if (LINK.LABEL) {
       if (!RightAbove){
